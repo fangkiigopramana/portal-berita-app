96705199f2d379ed062cf6dea4bd479d22bcb6b8
Fangki Igo Pramana <fangkiigo26@gmail.com>
2023-03-04 21:17:08 +0700

    second commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use League\CommonMark\Extension\CommonMark\Node\Block\ListItem;
+
+class HomeController extends Controller
+{
+    public function fetchData($newsApi = '/v1/cnn-news/', $type = '') {
+        $client = new Client();
+        $response = $client->request('GET', 'https://berita-indo-api.vercel.app'.$newsApi.$type);
+        $data = json_decode($response->getBody(), true);
+        return $data;
+    }
+
+    public function index()
+    {
+        $newsItems = $this->fetchData()['data'];
+        $listType = $this->fetchData('')['listApi']['CNN News']['listType'];
+        // dump($newsItems);
+        // dd($listType);
+        return view('home',[
+            'newsItems' => $newsItems,
+            'listType' => $listType
+        ]);
+    }
+
+
+}
